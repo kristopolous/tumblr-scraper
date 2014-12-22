2f3c2a16a1e3f0084dfef12c824bb3286742fb7c
Chris McKenzie <kristopolous@yahoo.com>
2014-12-22 15:13:26 -0800

    updating the exist

--- exist.sh
+++ exist.sh
@@ -4,12 +4,14 @@
 
 touch ~/exist.delisted.cache
 while read site; do
-   if [ `grep -c $site ~/exist.delisted.cache` = "0" ]; then
-     exist=`curl -s -I $site | head -1 | tr '\r' ' '`
-     if [ `echo $exist | grep -c 404` = "1" ]; then 
-       echo "$site" >> ~/exist.delisted.cache
-     else
-       echo $site
+   if [ ! -e /raid/tumblr/$site ]; then
+     if [ `grep -c $site ~/exist.delisted.cache` = "0" ]; then
+       exist=`curl -s -I $site | head -1 | tr '\r' ' '`
+       if [ `echo $exist | grep -c 404` = "1" ]; then 
+         echo "$site" >> ~/exist.delisted.cache
+       else
+         echo $site
+       fi
      fi
    fi
 done
